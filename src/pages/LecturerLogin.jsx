4c8ee3ff8322ed2b2cefdc0651359af201474f7d
Master <ss@>
2026-01-04 22:52:13 +0800

    fix: small ui fix

--- src/pages/LecturerLogin.jsx
+++ src/pages/LecturerLogin.jsx
@@ -84,7 +84,7 @@ function LecturerLogin() {
           <div>
             <button
               type="submit"
-              className="group relative w-full flex justify-center py-3 px-4 border border-transparent text-lg font-medium rounded-md text-black bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+              className="cursor-pointer group relative w-full flex justify-center py-3 px-4 border border-transparent text-lg font-medium rounded-md text-black bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
             >
               Sign In
             </button>
